acd0ad4df8f196402018d5b644c7ae59f698ba96
Dzianis Siarheyeu <dzianis_siarheyeu@epam.com>
2015-11-08 23:18:15 +0300

    Update description migration

--- db/migrate/20151108190743_add_description_to_quiz.rb
+++ db/migrate/20151108190743_add_description_to_quiz.rb
@@ -1,5 +1,5 @@
 class AddDescriptionToQuiz < ActiveRecord::Migration
   def change
-    add_column :quizzes, :description, :string
+    add_column :quizzes, :description, :string, default: ''
   end
 end
